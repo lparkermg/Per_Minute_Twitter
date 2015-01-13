4bbf1bafc5941f89c9c4a81fd9e862a0b8aa79d0
Luke Parker <luke.parker@bathasu.com>
2015-01-13 08:56:22 +0000

    Added frequency option to the Options Form.

--- PerMinuteTwitter/OptionsForm.Designer.cs
+++ PerMinuteTwitter/OptionsForm.Designer.cs
@@ -31,9 +31,16 @@
             this.label1 = new System.Windows.Forms.Label();
             this.searchTerm = new System.Windows.Forms.TextBox();
             this.gB1 = new System.Windows.Forms.GroupBox();
-            this.shortDuration = new System.Windows.Forms.RadioButton();
             this.longDuration = new System.Windows.Forms.RadioButton();
+            this.shortDuration = new System.Windows.Forms.RadioButton();
+            this.applyButton = new System.Windows.Forms.Button();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.freqGB = new System.Windows.Forms.GroupBox();
+            this.frequencyUpDown = new System.Windows.Forms.NumericUpDown();
             this.gB1.SuspendLayout();
+            this.freqGB.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.frequencyUpDown)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -64,17 +71,6 @@
             this.gB1.TabStop = false;
             this.gB1.Text = "Show Duration?";
             // 
-            // shortDuration
-            // 
-            this.shortDuration.AutoSize = true;
-            this.shortDuration.Location = new System.Drawing.Point(7, 20);
-            this.shortDuration.Name = "shortDuration";
-            this.shortDuration.Size = new System.Drawing.Size(50, 17);
-            this.shortDuration.TabIndex = 0;
-            this.shortDuration.TabStop = true;
-            this.shortDuration.Text = "Short";
-            this.shortDuration.UseVisualStyleBackColor = true;
-            // 
             // longDuration
             // 
             this.longDuration.AutoSize = true;
@@ -86,11 +82,85 @@
             this.longDuration.Text = "Long";
             this.longDuration.UseVisualStyleBackColor = true;
             // 
+            // shortDuration
+            // 
+            this.shortDuration.AutoSize = true;
+            this.shortDuration.Location = new System.Drawing.Point(7, 20);
+            this.shortDuration.Name = "shortDuration";
+            this.shortDuration.Size = new System.Drawing.Size(50, 17);
+            this.shortDuration.TabIndex = 0;
+            this.shortDuration.TabStop = true;
+            this.shortDuration.Text = "Short";
+            this.shortDuration.UseVisualStyleBackColor = true;
+            // 
+            // applyButton
+            // 
+            this.applyButton.Location = new System.Drawing.Point(256, 141);
+            this.applyButton.Name = "applyButton";
+            this.applyButton.Size = new System.Drawing.Size(95, 26);
+            this.applyButton.TabIndex = 3;
+            this.applyButton.Text = "Apply";
+            this.applyButton.UseVisualStyleBackColor = true;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(155, 141);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(95, 26);
+            this.okButton.TabIndex = 4;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(12, 141);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(95, 26);
+            this.cancelButton.TabIndex = 5;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // freqGB
+            // 
+            this.freqGB.Controls.Add(this.frequencyUpDown);
+            this.freqGB.Location = new System.Drawing.Point(15, 91);
+            this.freqGB.Name = "freqGB";
+            this.freqGB.Size = new System.Drawing.Size(336, 44);
+            this.freqGB.TabIndex = 6;
+            this.freqGB.TabStop = false;
+            this.freqGB.Text = "Frequency? (Minutes)";
+            // 
+            // frequencyUpDown
+            // 
+            this.frequencyUpDown.Location = new System.Drawing.Point(7, 18);
+            this.frequencyUpDown.Maximum = new decimal(new int[] {
+            600,
+            0,
+            0,
+            0});
+            this.frequencyUpDown.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.frequencyUpDown.Name = "frequencyUpDown";
+            this.frequencyUpDown.Size = new System.Drawing.Size(323, 20);
+            this.frequencyUpDown.TabIndex = 0;
+            this.frequencyUpDown.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            // 
             // OptionsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(363, 210);
+            this.ClientSize = new System.Drawing.Size(363, 179);
+            this.Controls.Add(this.freqGB);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.applyButton);
             this.Controls.Add(this.gB1);
             this.Controls.Add(this.searchTerm);
             this.Controls.Add(this.label1);
@@ -101,6 +171,8 @@
             this.Text = "OptionsForm";
             this.gB1.ResumeLayout(false);
             this.gB1.PerformLayout();
+            this.freqGB.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.frequencyUpDown)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -113,5 +185,10 @@
         private System.Windows.Forms.GroupBox gB1;
         private System.Windows.Forms.RadioButton longDuration;
         private System.Windows.Forms.RadioButton shortDuration;
+        private System.Windows.Forms.Button applyButton;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.GroupBox freqGB;
+        private System.Windows.Forms.NumericUpDown frequencyUpDown;
     }
 }
